3d33ad6f21b4b50bba6b7813b3f4c2df9937b74e
Tim <tim.travica@gmail.com>
2025-03-23 16:10:41 +0100

    updated .env in gitignore and added ssl statement to db connection

--- gym-progress-tracker/backend/server.js
+++ gym-progress-tracker/backend/server.js
@@ -10,12 +10,16 @@ app.use(express.json());
 
 // PostgreSQL-Verbindung
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'progress_tracker',
-    password: process.env.DB_PASSWORD || 'admin',
-    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER, // || 'postgres',
+    host: process.env.DB_HOST, //|| 'localhost',
+    database: process.env.DB_NAME, //|| 'progress_tracker',
+    password: process.env.DB_PASSWORD, //|| 'admin',
+    port: process.env.DB_PORT, //|| 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 
 // Verbindungstest
 pool.query('SELECT NOW()', (err, res) => {
